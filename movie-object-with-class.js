b9a05408f8efb84d6a4ee1e1c11349d68e38e8e6
ARBUCHELI <radio_seattle@hotmail.com>
2023-03-22 00:22:17 -0500

    adding movie object with class

--- movie-object-with-class.js
+++ movie-object-with-class.js
@@ -7,7 +7,7 @@ class Movie {
         this.rating = rating;
     }
 
-    getOverview = function() {
+    getOverview () {
         return `${this.title}, a ${this.genre} film directed by ${this.director} was released in ${this.releaseYear}. It received a rating of ${this.rating}`;
     }
 }
